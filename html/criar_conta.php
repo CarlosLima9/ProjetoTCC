33867601868ba21166807063b07a3a48270c87ca
unknown <grizantedavi@gmail.com>
2024-11-29 13:31:45 -0300

    Finalização do css da criação de conta

--- html/criar_conta.php
+++ html/criar_conta.php
@@ -35,6 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criar Conta</title>
+    <link rel="stylesheet" href="../css/criarConta.css">
 </head>
 <body>
     <h2>Criar Conta</h2>
